ac4180fb1fe31f29d55d84f8f8dd426dec8b48a0
Ivan Denlord C.Reyes(KPPH- RPB8N0002D) <PB8N0002@calcomp.com.tw>
2018-03-01 15:28:07 +0800

    add main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from ip.engine import  ip_function as get_ip
 
-query = input(int("What can I do for you? "))
-if query == 1:
-    return ip
+def ask(query):
+    if query == 1:
+        return get_ip()
+    else:
+        return print(None)
+    return None
+
+query = int(input("What can I do for you? "))
+ask(query)
