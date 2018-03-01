6d90f272943d84705b4b58dd80cb0c6aa0637ca1
Ivan Denlord C.Reyes(KPPH- RPB8N0002D) <PB8N0002@calcomp.com.tw>
2018-03-01 15:28:55 +0800

    fix add ip

--- ip/engine.py
+++ ip/engine.py
@@ -1,12 +1,13 @@
 import requests
 
-def ip():
-    ip = input("Enter IP")
+def ip_function():
+    ip = input("Enter IP:  ")
     print("Searching for IP...")
     ip_api = ("http://ip-api.com/json/"+ip)
-    data = request.get(ip_api)
+    data = requests.get(ip_api)
     ip_data = data.json()
     try:
         data = {"country": ip_data["country"], "isp": ip_data["isp"]}
     except KeyError:
          data = {"country": None, "isp": None}
+    return print(data)
